6e8770b6ecf884f2ec4c0a0f9512590f2c9798a2
Dani <dan.avila7@gmail.com>
2022-12-23 19:14:11 -0500

    v 0.0.3
    Readme.md

--- extension.js
+++ extension.js
@@ -7,20 +7,136 @@ const { XMLParser } = require('fast-xml-parser');
  */
 async function activate(context) {
 	let articles = [];
+	const parser = new XMLParser();
 	try {
-		const res = await axios.get("https://medium.com/feed/@dan.avila7", { 
-			headers: { "Accept-Encoding": "gzip,deflate,compress" } 
-		});
-		const parser = new XMLParser();
-		let json = parser.parse(res.data);
-		articles = json.rss.channel.item.map(article => {
-			return {
-				label: article.title,
-				detail: article.category[0],
-				link: article.link
+
+		//link1
+		const link1_url = vscode.workspace.getConfiguration().get('RSS.link1.url');
+		const link1_type = vscode.workspace.getConfiguration().get('RSS.link1.type');
+		if(link1_url != "")
+		{
+			const res = await axios.get(link1_url, { 
+				headers: { "Accept-Encoding": "gzip,deflate,compress" } 
+			});
+			let json = parser.parse(res.data);
+			let articles1 = json.rss.channel.item.map(article => {
+				return {
+					label: article.title,
+					detail: link1_type,
+					link: article.link
+				}
+			});
+			articles = articles.concat(articles1);
+			console.log(articles);
+		}
+
+		//link2
+		const link2_url = vscode.workspace.getConfiguration().get('RSS.link2.url');
+		const link2_type = vscode.workspace.getConfiguration().get('RSS.link2.type');
+		if(link2_url != "")
+		{
+			const res = await axios.get(link2_url, { 
+				headers: { "Accept-Encoding": "gzip,deflate,compress" } 
+			});
+			let json = parser.parse(res.data);
+			let articles2 = json.rss.channel.item.map(article => {
+				return {
+					label: article.title,
+					detail: link2_type,
+					link: article.link
+				}
+			});
+			articles = articles.concat(articles2);
+			console.log(articles);
+		}
+
+		//link3
+		const link3_url = vscode.workspace.getConfiguration().get('RSS.link3.url');
+		const link3_type = vscode.workspace.getConfiguration().get('RSS.link3.type');
+		if(link3_url != "")
+		{
+			const res = await axios.get(link3_url, { 
+				headers: { "Accept-Encoding": "gzip,deflate,compress" } 
+			});
+			let json = parser.parse(res.data);
+			let articles3 = json.rss.channel.item.map(article => {
+				return {
+					label: article.title,
+					detail: link3_type,
+					link: article.link
+				}
+			});
+			articles = articles.concat(articles3);
+			console.log(articles);
+		}
+
+		// //link4
+		const link4_url = vscode.workspace.getConfiguration().get('RSS.link4.url');
+		const link4_type = vscode.workspace.getConfiguration().get('RSS.link4.type');
+		if(link4_url != "")
+		{
+			const res = await axios.get(link4_url, { 
+				headers: { "Accept-Encoding": "gzip,deflate,compress" } 
+			});
+			let json = parser.parse(res.data);
+			let articles4 = [];
+			if(link4_type == 'Reddit')
+			{
+				articles4 = json.feed.entry.map(article => {
+					return {
+						label: article.title,
+						detail: link4_type,
+						link: article.link
+					}
+				});
 			}
-		});
-		console.log(articles);
+			else if(link4_type == 'Youtube')
+			{
+				
+				articles4 = json.feed.entry.map(article => {
+					let videoid = article.id.replace("yt:video:", "");
+					return {
+						label: article.title,
+						detail: link4_type,
+						link: 'https://www.youtube.com/watch?v='+videoid
+					}
+				});
+			}
+			else
+			{
+				articles4 = json.rss.channel.item.map(article => {
+					return {
+						label: article.title,
+						detail: link4_type,
+						link: article.link
+					}
+				});
+			}
+			
+			articles = articles.concat(articles4);
+			console.log(articles);
+		}
+
+		// //link5
+		const link5_url = vscode.workspace.getConfiguration().get('RSS.link5.url');
+		const link5_type = vscode.workspace.getConfiguration().get('RSS.link5.type');
+		if(link5_url != "")
+		{
+			const res = await axios.get(link5_url, { 
+				headers: { "Accept-Encoding": "gzip,deflate,compress" } 
+			});
+			let json = parser.parse(res.data);
+			let articles5 = json.rss.channel.item.map(article => {
+				return {
+					label: article.title,
+					detail: link5_type,
+					link: article.link
+				}
+			});
+			articles = articles.concat(articles5);
+			console.log(articles);
+		}
+		
 	} catch (error) {
 		console.error(error);
 	}
